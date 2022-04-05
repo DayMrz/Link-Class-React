9df3ae66ab4678a3b7da31f16ab052df386412db
Leily <leilymrz@gmail.com>
2022-04-05 00:11:07 -0700

    adding, removing, deleting

--- slot-machine-cases/Machine.js
+++ slot-machine-cases/Machine.js
@@ -1,58 +1,156 @@
+function pickFruit() {
+  const fruit = ['🍇', '🍊', '🍒']
+  return fruit[Math.floor(Math.random() * fruit.length)]
+}
+
+function generateSlots(slotsCount) {
+  return [...(new Array(slotsCount))].map(() => pickFruit())
+}
+
 class Machine extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      slots: generateSlots(props.slots)
+    }
+  }
+
+  play = () => {
+    this.setState((currentState) => {
+      return {
+        slots: currentState.slots.map(() => pickFruit())
+      }
+    })
+  }
+
+  static getDerivedStateFromProps = (props, state) => {
+    return props.slots !== state.slots.length ? { slots: generateSlots(props.slots) } : null
+  }
+
   render() {
-    const { slots } = this.props
+    const { slots } = this.state
+    const { play } = this
+    const { onDelete } = this.props
 
     if (slots.length <= 1) {
       return null
     }
-    
-    // let winner = true
-
-    // 1
-    // for (let i = 1; i < slots.length; i++) {
-    //   if (slots[i] !== slots[0]) {
-    //     winner = false
-    //   }
-    // }
-
-    // 2
-    // slots.forEach((currentSlot) => {
-    //   if (currentSlot !== slots[0]) {
-    //     winner = false
-    //   }
-    // })
-
-    // 3
-    // map [1,2,3] => [1,2,3]
-    // filter [1,2,3] => [] || [1] || [1,2] || [1,2,3]
-    // every [1,2,3] => boolean
-    // some [1,2,3] => boolean
-    // reduce [1,2,3] => any
-
-    // const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-    // const result = words.filter(word => word.length > 6);
-    // expected output: Array ["exuberant", "destruction", "present"]
 
     const winner = slots.every((currentSlot) => currentSlot === slots[0])
 
     return (
-    <div className="Machine">
-      <ul    
-      style={{display:'flex', listStyle: 'none'}}
-      className="list">
-        {slots.map((slot, index) => {
-          return <li key={index}>{slot}</li>
-        })}
-      </ul>
-      <p 
-        className={winner ? 'winner' : 'looser'}
-        style={{fontSize:'2em'}}>
-          {winner ? 'You Win!' : 'You Lose!'} 
-      </p>
-    </div>
+      <div className="Machine">
+        <ul
+          style={{ display: 'flex', listStyle: 'none' }}
+          className="list">
+          {slots.map((slot, index) => {
+            return <li key={index}>{slot}</li>
+          })}
+        </ul>
+        <p
+          className={winner ? 'winner' : 'looser'}
+          style={{ fontSize: '2em' }}
+        >
+          {winner ? 'You Win!' : 'You Lose!'}
+        </p>
+        <div style={{ display: 'flex', gap: 8 }}>
+          <button onClick={play}>Play</button>
+          <button
+            type="button"
+            onClick={onDelete}
+          >
+            Delete machine
+          </button>
+        </div>
+      </div>
     )
   }
 }
+
+// class Machine extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       slots: generateSlots(props.slots)
+//     }
+//   }
+
+//   play = () =>  {
+//     this.setState((currentState) => {
+//       return {
+//         slots: currentState.slots.map(() => pickFruit())
+//       }
+//     })
+//   }
+
+//   componentDidMount() {
+//     console.log('componentDidMount')
+//   }
+
+//   componentDidUpdate() {
+//     console.log('componentDidUpdate')
+//   }
+
+//   static getDerivedStateFromProps = (props, state) => {
+//     console.log('getDerivedStateFromProps', { props, state})
+
+//     return {
+//       slots: generateSlots(props.slots)
+//     }
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log('shouldComponentUpdate', { nextProps, nextState })
+//     return true
+//   }
+
+//   getSnapshotBeforeUpdate(prevProps, prevState) {
+//     console.log('getSnapshotBeforeUpdate', {
+//       prevProps,
+//       prevState,
+//       state: this.state,
+//       props: this.props
+//     })
+
+//     return null
+//   }
+
+//   componentWillUnmount = () => {
+//     console.log('Component will unmount')
+//   }
+
+//   render() {
+//     const { slots } = this.state
+//     const { play } = this
+
+//     if (slots.length <= 1) {
+//       return null
+//     }
+
+//     const winner = slots.every((currentSlot) => currentSlot === slots[0])
+
+//     return (
+//       <div className="Machine">
+//         <ul
+//           style={{ display: 'flex', listStyle: 'none' }}
+//           className="list">
+//           {slots.map((slot, index) => {
+//             return <li key={index}>{slot}</li>
+//           })}
+//         </ul>
+//         <p
+//           className={winner ? 'winner' : 'looser'}
+//           style={{ fontSize: '2em' }}
+//         >
+//           {winner ? 'You Win!' : 'You Lose!'}
+//         </p>
+//         <button onClick={play}>Play</button>
+//       </div>
+//     )
+//   }
+// }
 
 Machine.defaultProps = {
   slots: []
--- slot-machine-cases/index.js
+++ slot-machine-cases/index.js
@@ -1,23 +1,145 @@
-function pickFruit() {
-  const fruit = ['🍇', '🍊', '🍒']
-  return fruit[Math.floor(Math.random() * fruit.length)]
-}
+// class App extends React.Component {
+//   state = {
+//     slots: 2
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1 className="title">Slot Machine!</h1>
+
+//         <input
+//           type="number"
+//           onChange={(event) => {
+//             console.log(event)
+//             this.setState(() => {
+//               let slots = parseInt(event.target.value)
+
+//               if (slots < 2) {
+//                 slots = 2
+//               } else if (slots > 10) {
+//                 slots = 10
+//               }
+
+//               return {
+//                 slots,
+//               }
+//             })
+//           }}
+//           value={this.state.slots}
+//         />
+
+//         <Machine slots={this.state.slots} />
+//       </div>
+//     )
+//   }
+// }
+
+// class App extends React.Component {
+//   state = {
+//     machines: []
+//   }
+
+//   addMachine = () => {
+//     this.setState(currentState => {
+//       return {
+//         machines: [...currentState.machines, new Date().getTime()]
+//       }
+//     })
+//   }
+
+//   removeMachine = (machineId) => {
+//     this.setState(currentState => {
+//       return {
+//         machines: currentState.machines.filter(currentMachineId => currentMachineId !== machineId)
+//       }
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1 className="title">Slot Machine!</h1>
+
+//         {this.state.machines.map((machineId, index) => {
+//           return <div key={machineId} style={{ padding: 10 }}>
+//             <Machine slots={index + 2} />
+
+//             <button
+//               type="button"
+//               onClick={
+//                 () => this.removeMachine(machineId)
+//               }
+//             >
+//               Delete machine
+//             </button>
+//           </div>
+//         })}
+
+//         <button type="button" onClick={() => this.addMachine()}>Add machine</button>
+//       </div>
+//     )
+//   }
+// }
 
 class App extends React.Component {
+  state = {
+    machines: [],
+    slotInput: 2,
+  }
+
+  addMachine = () => {
+    this.setState(currentState => {
+      return {
+        machines: [
+          ...currentState.machines,
+          {
+            id: new Date().getTime(),
+            slots: currentState.slotInput
+          }
+        ]
+      }
+    })
+  }
+
+  removeMachine = (machineId) => {
+    this.setState(currentState => {
+      return {
+        machines: currentState.machines.filter((currentMachine) => currentMachine.id !== machineId)
+      }
+    })
+  }
+
   render() {
-
-    return ( 
+    return (
       <div>
-        <h1 className="title"> Slot Machine!</h1>
+        <h1 className="title">Slot Machine!</h1>
 
-        <Machine />
-        <Machine slots={[pickFruit()]} />
-        <Machine 
-          slots={[pickFruit(), pickFruit()]}
+        <input
+          type="number"
+          onChange={(event) => {
+            this.setState(() => {
+              let slots = parseInt(event.target.value)
+
+              if (slots < 2) {
+                slots = 2
+              } else if (slots > 10) {
+                slots = 10
+              }
+
+              return {
+                slotInput: slots,
+              }
+            })
+          }}
+          value={this.state.slotInput}
         />
-        <Machine 
-          slots={[pickFruit(), pickFruit(), pickFruit(), pickFruit(), pickFruit()]}
-        />
+
+        {this.state.machines.map(({ id, slots }) => {
+          return <Machine key={id} slots={slots} onDelete={() => this.removeMachine(id)} />
+        })}
+
+        <button type="button" onClick={() => this.addMachine()}>Add machine</button>
       </div>
     )
   }
